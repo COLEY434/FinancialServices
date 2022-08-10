aa9ba45c788f0f9124d6621534c1ccbe5a9b9bc1
collins Anieze <chigoziecollins434@gmail.com>
2022-08-10 14:30:15 +0100

    Created the account and customer domain entity.

---
+++ AccountService/src/AccountService.Core/Entities/Account.cs
@@ -0,0 +1,20 @@
+﻿using AccountService.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountService.Core.Entities
+{
+    public class Account
+    {
+        public string AccountNo { get; set; }
+        public char AccountType { get; set; }
+        public string CustomerID { get; set; }
+        public AccountStatus AccountStatus { get; set; }
+        public DateTime? ActivationDate { get; set; }
+        public DateTime CreationDate { get; set; }
+        public decimal AvaliableBalance { get; set; }
+    } 
+}
